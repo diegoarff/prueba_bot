5f844d5b529fb4adce931c16e37c712f4affd148
diegoarff <diegorinconf1@gmail.com>
2022-07-07 14:58:07 -0400

    update sendMail.js

--- functions_bot/sendMail.js
+++ functions_bot/sendMail.js
@@ -1,8 +1,10 @@
-const nodemailer = require('nodemailer')
+const nodemailer = require('nodemailer');
+var i18n = require("i18n");
 
 const { API_DB, ENDPOINTS_CARTS } = require('~inst');
 
 const viewCart = require('./viewCart');
+const { __ } = require('i18n');
 
 async function sendMail(userId) {
 
@@ -19,10 +21,10 @@ async function sendMail(userId) {
         let transporter = nodemailer.createTransport({
             host: "smtp.gmail.com",
             port: 587,
-            secure: false, // true for 465, false for other ports
+            secure: false, 
             auth: {
-                user: 'equipo7socialoplesk@gmail.com', // generated ethereal user
-                pass: 'otggpxhtyarvecmg', // generated ethereal password
+                user: 'equipo7socialoplesk@gmail.com', 
+                pass: 'otggpxhtyarvecmg', 
             },
         });
     
@@ -33,16 +35,10 @@ async function sendMail(userId) {
         }
     
         let info = await transporter.sendMail({
-            from: '"Foo Market 6" <equipo7socialoplesk@gmail.com>', // sender address
-            to: `${email}, equipo7socialoplesk@gmail.com`, // list of receivers
-            subject: "Tu factura de compra en Foo Market 6 ✔", // Subject line
-            text: `Gracias por comprar en Foo Market 6!\nAquí tiene su factura:\n\n` +
-                  `> Date: ${date()}\n` +
-                  `> Nombre: ${firstName}\n` +
-                  `> Apellido: ${lastName}\n` +
-                  `> Ubicacion: ${locations[location - 1]}\n` +
-                  `> Metodo de pago: ${pay_methods[payment_method - 1]}\n\n` +
-                  `> Productos:\n\n${msg}` // html body
+            from: '"Foo Market 6" <equipo7socialoplesk@gmail.com>', 
+            to: `${email}, equipo7socialoplesk@gmail.com`, 
+            subject: "Tu factura de compra en Foo Market 6 ✔", 
+            text: __('sendMailText', firstName, lastName, locations[location - 1], pay_methods[payment_method - 1], msg)
             },
             function (error) {
                 if (error) {
